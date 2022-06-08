319b5b61249e64780fe5cea40b6564d2bcbf0df1
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-08 16:55:57 +0600

    load data from api use useUserhook

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import useUsers from './useUsers';
 
 const Home = () => {
+    const [users] = useUsers()
+
     return (
         <div>
+            <h2>{users?.length}</h2>
+            <div>
+
+            </div>
 
         </div>
     );
