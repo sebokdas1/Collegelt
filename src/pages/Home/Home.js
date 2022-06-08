6c22ee8119e8a50a0dabe23802bcfa0e74e2f453
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-08 17:24:48 +0600

    show data on table

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,8 +7,36 @@ const Home = () => {
     return (
         <div>
             <h2>{users?.length}</h2>
-            <div>
 
+            <div className='container mx-auto'>
+                <div class="overflow-x-auto">
+                    <table class="table table-compact w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Gender</th>
+                                <th>City</th>
+                                <th>State</th>
+                                <th>Country</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((user, index) =>
+                                    <tr key={user.email} className="hover">
+                                        <th>{index + 1}</th>
+                                        <td>{user?.name?.title} {user.name.first} {user?.name?.last}</td>
+                                        <td>{user?.gender}</td>
+                                        <td>{user?.location?.city}</td>
+                                        <td>{user?.location?.state}</td>
+                                        <td>{user?.location?.country}</td>
+                                    </tr>)
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </div>
